4d22d16b2a9b63b89cea48f509f8dd89db30ca63
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-12-19 23:05:48 +0600

    socket server modify

--- src/socket-server.ts
+++ src/socket-server.ts
@@ -19,21 +19,54 @@ const entryALog = (payload:TLog):TLog=>{
 
 export const activeSocketServer =(io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>)=>{
     io.on('connection', (socket) => {
-        const id  = socket.handshake.query.id
-        console.log(id,socket.id)
-        socket.emit('joined-to-edit', entryALog({
-            type: 'alert',
-            message: "A new user joined to edit.",
-            receiver:null,
-            sender:'system'
-        }))
+        const {id,
+            name,
+            roomNumber
+        }  = socket.handshake.query
+        console.log(id,name,roomNumber)
 
-        socket.on('sendMessage',(payload:TLog)=>{
-            socket.emit('receiveMessage', `Received message from ${payload.sender}`)
+        let rooms:string[] =[]
+        // if (rooms.find((room)=> room === roomNumber)){
+        //     socket.join(roomNumber as  string)
+        // }
+        socket.on('joinRoom', ({name,roomNumber}) => {
+            console.log('join room',{name,roomNumber})
+            socket.join(roomNumber as string);
+            console.log(`User ${name} joined this room.`);
+
+            // Notify other users in the room
+            io.to(roomNumber).emit('userJoined', entryALog({
+                type: 'alert',
+                message: `${name} joined this room.`,
+                receiver:null,
+                sender:'System'
+            }));
+        });
+
+        // socket.emit('joined-to-edit', entryALog({
+        //     type: 'alert',
+        //     message: `${name} joined this room.`,
+        //     receiver:null,
+        //     sender:'System'
+        // }))
+
+        socket.on('sendMessage',(payload:{
+            roomNumber:string,
+            data:TLog
+        })=>{
+            console.log('send message',{payload})
+            // socket.emit('receiveMessage', `Received message from ${payload.sender}`)
+            io.to(payload.roomNumber).emit('message',payload.data);
         })
 
         socket.on('disconnect',(reason)=>{
             console.log(`user disconnected. Id ${socket.id}.  ${reason}`)
+            io.to(roomNumber as string).emit('userJoined', entryALog({
+                type: 'alert',
+                message: `${name as string} disconnected. reason- ${reason}`,
+                receiver:null,
+                sender:'System'
+            }));
         })
     });
 
