21cb801705c3d993e22f0354d0a6ac4fbfe56000
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-12-19 17:01:32 +0600

    cors update

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ const io = new Server(server,{
     },
     cors:{
         origin:[
-            'http://localhost:3000'
+            'http://localhost:3000',
+            'https://docs-sharing.trelyt.store'
         ]
     }
 });
